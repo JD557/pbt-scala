7937e460510add87884caa7a32739e8aa84617f8
João Costa <jdpc557@gmail.com>
2019-06-02 10:10:26 +0100

    Improve Unit Tests

--- src/main/scala/eu/joaocosta/pbt/props/UnitTests.scala
+++ src/main/scala/eu/joaocosta/pbt/props/UnitTests.scala
@@ -5,8 +5,9 @@ import eu.joaocosta.pbt.props.Impls._
 object UnitTests extends App {
   def test(fun: String => Boolean) {
     assert(fun("") == true)
-    assert(fun("asd") == true)
-    assert(fun("Asd") == true)
+    assert(fun("aefioafsio") == true)
+    assert(fun("IHAU") == true)
+    assert(fun("ApBoSiA") == true)
     assert(fun("0123") == false)
     assert(fun("   ") == false)
     assert(fun("01asd") == false)
